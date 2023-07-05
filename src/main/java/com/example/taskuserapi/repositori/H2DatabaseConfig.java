fa38487c859bfdb1bd76ea74242b0d06c41454cc
Oleksii Shykhov <tema.sh125@gmail.com>
2023-07-05 19:40:16 +0300

    folder structure created and H2 database connected

--- src/main/java/com/example/taskuserapi/repositori/H2DatabaseConfig.java
+++ src/main/java/com/example/taskuserapi/repositori/H2DatabaseConfig.java
@@ -1,2 +1,10 @@
-package com.example.taskuserapi.repositori;public class H2DatabaseConfig {
+package com.example.taskuserapi.repositori;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class H2DatabaseConfig {
+
 }
